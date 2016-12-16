59fdd40cca5398554db52114cd781c62355bf09f
Schuyler Alschuler <Schuyler Alschuler>
2016-12-15 23:19:25 -0800

    Setup complete

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
 
 class App extends Component {
+
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyBLQzxyCfHSlfa5caT5T8sR6TeLX1Ppd0I',
+      authDomain: 'manager-56756.firebaseapp.com',
+      databaseURL: 'https://manager-56756.firebaseio.com',
+      storageBucket: 'manager-56756.appspot.com',
+      messagingSenderId: '322889087413'
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
-    return(
+    return (
       <Provider store={createStore(reducers)}>
         <View>
           <Text>Hello</Text>
